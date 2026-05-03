0e33ecf5f014eefc3bdf6cf676773f8470072bf1
kidsengt <kidsengt@gmail.com>
2026-05-03 22:56:53 +0900

    Update stage3.js

--- books/stage3.js
+++ books/stage3.js
@@ -133,14 +133,14 @@ words: [
     words: [
       { en: "came", ko: "왔다" },
       { en: "made", ko: "만들었다" },
-      { en: "cake", ko: "케이크" },
+      { en: "looked for", ko: "찾았다" },
       { en: "eggs", ko: "달걀들" },
       { en: "idea", ko: "생각" },
       { en: "park", ko: "공원" },
-      { en: "trees", ko: "나무들" },
+      { en: "went to", ko: "~에 갔다" },
       { en: "flowers", ko: "꽃들" },
       { en: "squirrels", ko: "다람쥐들" },
-      { en: "looked", ko: "보았다" }
+      { en: "had", ko: "가지고 있었다, 가져갔" }
     ]
   },
   "s3-2-3": {
@@ -149,7 +149,7 @@ words: [
     book: 3,
     title: "Nobody Wanted to Play",
     words: [
-      { en: "park", ko: "공원" },
+      { en: "played", ko: "놀았다" },
       { en: "rocket", ko: "로켓" },
       { en: "see-saw", ko: "시소" },
       { en: "horse", ko: "말" },
@@ -157,8 +157,8 @@ words: [
       { en: "ladder", ko: "사다리" },
       { en: "wall", ko: "벽" },
       { en: "spaceman", ko: "우주인" },
-      { en: "juggler", ko: "저글러" },
-      { en: "cowboy", ko: "카우보이" }
+      { en: "climbed up", ko: "올라갔다" },
+      { en: "cross", ko: "화" }
     ]
   },
   "s3-2-4": {
@@ -174,7 +174,7 @@ words: [
       { en: "ladder", ko: "사다리" },
       { en: "climbed down", ko: "내려왔다" },
       { en: "put", ko: "놓았다" },
-      { en: "jumped", ko: "뛰어내렸다" },
+      { en: "jumped down", ko: "뛰어내렸다" },
       { en: "fireman", ko: "소방관" },
       { en: "everyone", ko: "모두" }
     ]
@@ -191,10 +191,10 @@ words: [
       { en: "rope", ko: "밧줄" },
       { en: "climbed", ko: "올라갔다" },
       { en: "pushed", ko: "밀었다" },
-      { en: "looked", ko: "보았다" },
+      { en: "looked at", ko: "보았다" },
       { en: "barked", ko: "짖었다" },
       { en: "went", ko: "갔다" },
-      { en: "home", ko: "집" }
+      { en: "silly", ko: "바보 같" }
     ]
   },
   "s3-2-6": {
@@ -203,16 +203,16 @@ words: [
     book: 6,
     title: "By the Stream",
     words: [
-      { en: "rug", ko: "돗자리" },
+      { en: "went", ko: "갔다" },
       { en: "stream", ko: "개울" },
       { en: "bridge", ko: "다리" },
       { en: "sticks", ko: "나뭇가지들" },
       { en: "water", ko: "물" },
-      { en: "climbed", ko: "올라갔다" },
+      { en: "climbed up", ko: "올라갔다" },
       { en: "dropped", ko: "떨어뜨렸다" },
       { en: "fell", ko: "떨어졌다" },
       { en: "Teddy", ko: "테디 인형" },
-      { en: "get", ko: "가져오다" }
+      { en: "couldn't get", ko: "가져오지 못다" }
     ]
   },
 "s3-3-1": {
